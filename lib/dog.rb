be6050874b89fcca2e691f8273e0046ee86864b9
Petya Djanovska <petya.djanovska@gmail.com>
2018-03-23 16:31:11 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,6 +65,16 @@ class Dog
     Dog.new(id: row[0], name: row[1], breed: row[2])
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM dogs WHERE dogs.id = ?
+    SQL
+
+    row = DB[:conn].execute(sql, id).flatten
+    Dog.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+
   def self.find_or_create_by(name:, breed:)
   dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
   if !dog.empty?
